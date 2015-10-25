f9c7a08d7f8df798270c863a77c5d9ec3fa2bcf3
Sijie Tian <sijie@socialtables.com>
2015-10-25 11:40:45 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import hammersley from "hammersley";
+const PI = Math.PI;
+
+export function hemisphereSampleUniform(i, N) {
+  const hPoint = hammersley(i, N);
+  const u = hPoint[0];
+  const v = hPoint[1];
+  const phi = v * 2.0 * PI;
+  const cosTheta = 1.0 - u;
+  const sinTheta = Math.sqrt(1.0 - cosTheta * cosTheta);
+  return [Math.cos(phi) * sinTheta, Math.sin(phi) * sinTheta, cosTheta];
+}
+
+export function hemisphereSampleCos(i, N) {
+  const hPoint = hammersley(i, N);
+  const u = hPoint[0];
+  const v = hPoint[1];
+  const phi = v * 2.0 * PI;
+  const cosTheta = Math.sqrt(1.0 - u);
+  const sinTheta = Math.sqrt(1.0 - cosTheta * cosTheta);
+  return [Math.cos(phi) * sinTheta, Math.sin(phi) * sinTheta, cosTheta];
+}
